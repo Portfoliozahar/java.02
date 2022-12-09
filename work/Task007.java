300ae5970757cdf4228dc70ec95595b782ac1be9
borodinets <assassqwas@gmail.com>
2022-12-09 11:54:43 +0500

    7a

--- work/Task007.java
+++ work/Task007.java
@@ -3,18 +3,29 @@ package work;
 import java.util.Scanner;
 
 public class Task007 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
-        System.out.println("Введите размер массива: ");
-        int n = input.nextInt();
+      
+    public static int num() {
+        Scanner input = new Scanner(System.in);
+            System.out.println("Введите число: ");
+            int n = input.nextInt(); 
+            return n;
+    }
+    public static int func(int n){
+        return n * (n + 1) / 2;
     }
 
-    public static int func(int n){
-            return n * (n + 1) / 2;
-        }
+    public static void res(int n, int resault){
+        System.out.println(n + " треугольное число равно: " + resault);
+    }
 
-    public static void showResult(int n, int result){
-        System.out.println(n + "-е треугольное число равно: " + result);
-}
+    public static void main(String[] args) {
+        int n = num();
+        int res = func(n);
+        res(n, res);
+    }
 }
 
+
+    
+
+       
